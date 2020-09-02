4cf03539421b32de5a10b4cdd3ec74eb2b0bfd98
Nillkizz <kotovvsan@ya.ru>
2020-09-02 08:43:09 +0300

    cleaned chat code and pretify chat interface + add downscrolling on load and new message

--- project/chat/api.py
+++ project/chat/api.py
@@ -9,7 +9,6 @@ def get_last_messages(request):
         room_group_name = 'mainROOM'
         redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
                                            port=settings.REDIS_PORT, db=0)
-        print(redis_instance.exists(f'chat_{room_group_name}_messages'))
         if redis_instance.exists(f'chat_{room_group_name}_messages') == 1:
             message_list = redis_instance.get(f'chat_{room_group_name}_messages').decode()
             return JsonResponse({'message_list': message_list})
