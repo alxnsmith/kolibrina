c40e8b004eabb11cb126f3399d9d2f4a6eb5fb0c
Nillkizz <kotovvsan@yandex.ru>
2020-10-03 11:46:23 +0300

    Переработан онлайн, теперь это не количество соединений, а количество авторизованыых пользователей. Так же теперь хранит список польззователей онлайн и количество открытых вкладок с сайтом

--- project/channel_common/consumers.py
+++ project/channel_common/consumers.py
@@ -1,39 +1,42 @@
-import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-import redis
-from django.conf import settings
 
 
 class OnlineConsumer(WebsocketConsumer):
-    """Счетчик онлайна (Сейчас счиает кол-во соединений с этим каналом, стоит он в хэдере)"""
-    redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
-                                       port=settings.REDIS_PORT, db=0)
-    redis_instance.set('online', 0)
+    online_list = {}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.username = self.scope['user']
 
     def connect(self):
+        self.username = str(self.username)
         async_to_sync(self.channel_layer.group_add)(
             'online',
             self.channel_name
         )
-        self.redis_instance.incr('online')
-        async_to_sync(self.channel_layer.group_send)(
-            'online',
-            {'type': 'new_online',
-             'value': str(self.redis_instance.get('online').decode())}
-        )
         self.accept()
-        self.send(self.redis_instance.get('online').decode())
+        if self.online_list.get(self.username):
+            self.online_list[self.username] += 1
+        elif self.username != 'AnonymousUser':
+            self.online_list[self.username] = 1
+        self.send_online()
 
     def disconnect(self, code):
-        self.redis_instance.decr('online')
-        async_to_sync(self.channel_layer.group_send)(
-            'online',
-            {'type': 'new_online',
-             'value': str(self.redis_instance.get('online').decode())}
-        )
-        self.send(json.dumps({'online': self.redis_instance.get('online').decode()}))
+        if self.username != 'AnonymousUser':
+            self.send_online()
+            if self.online_list.get(self.username):
+                self.online_list[self.username] -= 1
+                if self.online_list[self.username] == 0:
+                    del self.online_list[self.username]
 
     def new_online(self, event):
         value = event['value']
         self.send(value)
+
+    def send_online(self):
+        async_to_sync(self.channel_layer.group_send)(
+            'online',
+            {'type': 'new_online',
+             'value': str(len(self.online_list))}
+        )
