2ee3451a30dd782c5b4f8e4cbaa45a1cf225044d
Nillkizz <kotovvsan@yandex.ru>
2021-01-05 15:22:02 +0300

    renamed "orderRules" name field in Rule app to "priority"

--- project/rules/models.py
+++ project/rules/models.py
@@ -5,14 +5,14 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Rule(models.Model):
-    orderRules = models.CharField(max_length=100, verbose_name='Порядок', default='')
+    priority = models.CharField(max_length=100, verbose_name='Порядок', default='')
     ruleTitle = models.CharField(max_length=100, verbose_name='Название', default='')
     ruleDescription = models.TextField(verbose_name='Полный текст', default='')
 
     def __str__(self):
-        return '{0}. {1}'.format(self.orderRules, self.ruleTitle)
+        return '{0}. {1}'.format(self.priority, self.ruleTitle)
 
     class Meta:
         verbose_name = _('Правило')
         verbose_name_plural = _('Правила')
-        ordering = ('orderRules',)
+        ordering = ('priority',)
--- project/rules/views.py
+++ project/rules/views.py
@@ -3,6 +3,6 @@ from .models import Rule
 
 
 def rules(request):
-    rulesList = Rule.objects.extra(select={'order': 'CAST(orderRules AS INTEGER)'}).order_by('order')
+    rulesList = Rule.objects.extra(select={'order': 'CAST(priority AS INTEGER)'}).order_by('order')
 
     return render(request, 'rules/rules.html', {'rules': rulesList})
